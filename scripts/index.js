13c826b99971577a4d270ade6bcaedbf4db70abb
Lima <felipebr4095@gmail.com>
2025-06-26 22:56:10 -0300

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -118,6 +118,7 @@ window.onload = () => {
             else Swal.fire('Erro', 'Erro ao consultar.', 'error');
         },
         agent: () => window.location.href = "/agent",
+        gitea: () => window.location.href = "/git",
         drive: () => window.location.href = "/drive",
         options: () => window.location.href = "options",
         security: () => window.location.href = "security"
