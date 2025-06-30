2014a88a47a7bcc44df80672fae103e3926dff37
Mr Lima <felipebr4095@gmail.com>
2025-06-30 11:11:05 -0300

    148

--- scripts/index.js
+++ scripts/index.js
@@ -241,7 +241,7 @@ window.onload = () => {
                     }).then((result) => {
                         if (result.isConfirmed) {
                             const content = result.value;
-                            const { status } = await fetchRequest("send", { to: autor, content });
+                            const { status } = fetchRequest("send", { to: autor, content });
                             if (status == 200) Swal.fire('Sucesso', 'Sua mensagem foi enviada!', 'success');
                             else if (status == 404) Swal.fire('Erro', 'O destinatário não foi encontrado!', 'error');
                             else Swal.fire('Erro', 'Erro ao enviar mensagem.', 'error');
