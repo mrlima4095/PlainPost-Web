11b409f39dcda7b29d3e9528b7bcb71c32990dbd
Mr Lima <felipebr4095@gmail.com>
2025-07-03 21:26:33 -0300

    bold

--- scripts/index.js
+++ scripts/index.js
@@ -134,7 +134,7 @@ window.onload = () => {
 
             const { status } = await fetchRequest("block", { user_to_block: user });
 
-            if (status === 200) Swal.fire({ title: 'Sucesso', text: `Usuário '${user}' bloqueado!`, icon: 'success' });
+            if (status === 200) Swal.fire({ title: 'Sucesso', text: `Usuário <strong>${user}<strong> bloqueado!`, icon: 'success' });
             else if (status === 404) Swal.fire({ title: 'Erro', text: 'Usuário não encontrado.', icon: 'error' });
             else if (status === 405) Swal.fire({ title: 'Erro', text: 'Você não pode bloquear você mesmo.', icon: 'error' });
             else if (status === 409) Swal.fire({ title: 'Atenção', text: 'Usuário já está bloqueado.', icon: 'info' });
